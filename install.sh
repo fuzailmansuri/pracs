f6ada781bf2ce937d9f1cc217f55f148912bc0d5
fuzailmansuri <130100626+fuzailmansuri@users.noreply.github.com>
2025-02-09 01:21:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
-# Exit on error
-set -e
+set -e  # Exit on error
 
-# Define user
-USER_NAME="fuzail"  # Change if needed
+# Update system
+sudo pacman -Syu --noconfirm
 
-echo "Updating system and installing required packages..."
-pacman -Sy --noconfirm hyprland xorg-xwayland xorg-xinit sddm \
-  waybar rofi kitty neofetch network-manager-applet \
-  pipewire pipewire-pulse wireplumber pulseaudio-bluetooth \
-  git neovim ttf-inter
+# Install essential packages
+sudo pacman -S --noconfirm \
+    networkmanager wpa_supplicant wireless_tools iwd dhclient \
+    bluez bluez-utils \
+    pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber \
+    xdg-user-dirs xdg-utils \
+    mesa vulkan-intel \
+    git curl wget unzip zip tar \
+    nano vim neofetch htop \
+    sudo
 
-# Enable SDDM
-echo "Enabling SDDM..."
-systemctl enable sddm
+# Enable services
+sudo systemctl enable --now NetworkManager
+sudo systemctl enable --now bluetooth
 
-# Set up .xinitrc for Hyprland
-echo "Setting up .xinitrc..."
-echo "exec Hyprland" > /home/$USER_NAME/.xinitrc
-chmod +x /home/$USER_NAME/.xinitrc
-chown $USER_NAME:$USER_NAME /home/$USER_NAME/.xinitrc
+# Install Hyprland and dependencies
+sudo pacman -S --noconfirm \
+    hyprland waybar rofi dunst alacritty \
+    thunar thunar-volman tumbler gvfs \
+    polkit-gnome xdg-desktop-portal-hyprland \
+    firefox
 
-# Install dotfiles
-echo "Setting up dotfiles..."
-git clone https://github.com/fuzailmansuri/dotfiles /home/$USER_NAME/.config
-chown -R $USER_NAME:$USER_NAME /home/$USER_NAME/.config
+# Set default scaling to 1.25x
+echo "Xft.dpi: 120" > ~/.Xresources
+echo "export GDK_SCALE=1.25" >> ~/.bashrc
+echo "export QT_SCALE_FACTOR=1.25" >> ~/.bashrc
 
-# Set 1.25x scaling
-echo "Applying 1.25x scaling..."
-echo "Xft.dpi: 120" >> /home/$USER_NAME/.Xresources
-chown $USER_NAME:$USER_NAME /home/$USER_NAME/.Xresources
+# Set up Hyprland Material You theme
+git clone https://github.com/koeqaife/hyprland-material-you.git ~/.config/hypr
 
-# Enable NetworkManager
-echo "Enabling NetworkManager..."
-systemctl enable NetworkManager
+# Install SDDM (GUI login)
+sudo pacman -S --noconfirm sddm qt5-graphicaleffects qt5-svg
+sudo systemctl enable --now sddm
 
-echo "Installation complete. Reboot now."
+echo "Setup complete! Reboot your system."
